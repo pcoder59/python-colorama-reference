3b6d60252f19709913ade5a56f390e83723b94db
premraj59 <premraj59128@protonmail.com>
2020-12-28 13:48:15 +0530

    autoreset false

--- colorama-learn.py
+++ colorama-learn.py
@@ -5,3 +5,5 @@ from colorama import Fore, Back, Style
 colorama.init()
 
 print("Fore.RED: " + Fore.RED + "This Will Print Red Text")
+print("This will be Red if autoreset is not True in init()")
+print("\033[39m")
